004f8d0c1482b0ce9b8f369dc50666604b72f79c
JaspherJ <jazoswald@gmail.com>
2018-04-15 20:58:48 -0400

    dynamically add the ability to add new cards

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Card = (props) => {
+return (
+          <div>
+              <img src = {props.avatar}/>
+              <div>
+                <div>{props.name} </div>
+                <div>{props.company}</div>
+              </div> 
+          </div>
+        );
+};
+
+let data = [
+  {
+    name : "Jaspher",
+    avatar :"https://avatars0.githubusercontent.com/u/4433303?v=4",
+    company:"Fl"
+  },
+  {
+    name : "John",
+    avatar :"https://avatars1.githubusercontent.com/u/1668?v=4",
+    company:"Syncfusion"
+  }
+]
+const CardList = (props) => {
+    return (
+      <div>
+        {props.cards.map(cards => <Card  {...cards}/>)}
+       
+      </div>
+    );
+};
+class Form extends React.Component {
+
+  render(){
+    return (
+      <form>
+        <input type = "text" placeholder = "Github Username" />
+        <button>Add Card </button>
+      </form>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div> <Form /></div>
+        <div><CardList cards = {data}/> </div>
+      </div>
+    );
+  }
+}
+
+export default App;
